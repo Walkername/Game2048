9cc7a500456654017ae8676f2d492c18e97fcdc0
Timur Magomedov <timurmag2020@gmail.com>
2021-05-24 09:07:49 +0300

    Leftovers

--- src/test/java/logic_pack/TestLogic.java
+++ src/test/java/logic_pack/TestLogic.java
@@ -6,20 +6,18 @@ import org.junit.Test;
 public class TestLogic {
 
     @Test
-    public void logicConstructor() {
-        Logic.initialState();
-        Assert.assertEquals(0, Logic.gameBoard.getState(1,3));
-    }
-
-    @Test
     public void testCreateCells() {
-        Logic.initialState();
-        Logic.createCells();
+
         int state = 2;
         String path = "assets/" + state + ".png";
         String path1 = "assets/2.png";
         Assert.assertEquals(path1, path);
 
+        Logic.initialState();
+        Logic.createCells();
+        Logic.createCells();
+        Logic.createCells();
+        Logic.createCells();
         int numberCells = 0;
         int[][] board = Logic.gameBoard.board;
         for (int i = 0; i < board.length; i++) {
@@ -27,7 +25,7 @@ public class TestLogic {
                 if (board[i][j] == 2) numberCells++;
             }
         }
-        Assert.assertEquals(2, numberCells);
+        Assert.assertEquals(8, numberCells);
     }
 
     @Test
@@ -54,9 +52,15 @@ public class TestLogic {
         board[0][2] = 128;
         board[3][2] = 128;
         board[3][0] = 256;
+        board[2][0] = 16;
+        board[0][3] = 4;
+        board[1][0] = 16;
+        board[3][3] = 4;
         Logic.checkShift(Direction.RIGHT);
         Logic.checkShift(Direction.DOWN);
         Assert.assertEquals(512, board[3][0]);
+        Assert.assertEquals(32, board[2][0]);
+        Assert.assertEquals(8, board[3][1]);
     }
 
 }
